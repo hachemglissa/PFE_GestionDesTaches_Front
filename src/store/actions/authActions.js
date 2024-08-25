1034bc916e9a177b5fecc258dc9f9deab16aa707
hachemglissa <hachemglissa74@gmail.com>
2024-08-25 18:13:19 +0100

    api modified

--- src/store/actions/authActions.js
+++ src/store/actions/authActions.js
@@ -5,7 +5,7 @@ export const login = (email, password) => {
   }
   return async (dispatch) => {
     try {
-      await fetch('https://localhost:7220/api/users/login', {
+      await fetch('https://localhost:7039/api/users/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -32,7 +32,7 @@ export const register = (email, username, password, role) => {
   return async (dispatch) => {
     try {
       const response = await fetch(
-        'https://localhost:7220/api/users/register',
+        'https://localhost:7039/api/users/register',
         {
           method: 'POST',
           headers: {
@@ -58,7 +58,7 @@ export const projet = (name, prescription, startDate, endDate) => {
   console.log('token', token)
   return async (dispatch) => {
     try {
-      const response = await fetch('https://localhost:7220/api/projets/new', {
+      const response = await fetch('https://localhost:7039/api/projets/new', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -76,7 +76,7 @@ export const projetAll = () => {
     var token = localStorage.getItem('token')
     dispatch({ type: 'PROJETS_LOAD_REQUEST' })
     try {
-      const response = await fetch('https://localhost:7220/api/projets', {
+      const response = await fetch('https://localhost:7039/api/projets', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
